480497bbd555f7b29ab26f612cbd5ca75c2efd8f
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-12 22:32:08 +0000

    fix(changelog): use next tag for changelog version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 set -e
 
-git status
-git clean -f
-git status
-
 REPO_URL=$(git config --get remote.origin.url | sed 's/\.git//g' | sed 's/\/\/.*:.*@/\/\//g')
-APP_NAME=${REPO_URL##*/}
-
-echo "git-semver-tags"
-OLD_TAG=$(git-semver-tags | sed '1 ! d')
-echo $OLD_TAG
-
-echo "conventional-recommended-bump"
+APP_NAME= ${REPO_URL##*/}
+PREVIOUS_TAG=$(git-semver-tags | sed '1 ! d')
 INCREMENT=$(conventional-recommended-bump -p angular)
-echo $INCREMENT
+NEXT_TAG=`semver $PREVIOUS_TAG -i $INCREMENT`
 
-echo "git-changelog"
-git-changelog -a $APP_NAME -n $OLD_TAG -r $REPO_URL
+git-changelog -a $APP_NAME -n $NEXT_TAG -r $REPO_URL
 cat CHANGELOG.md
-
-echo "semver"
-NEXT_TAG=`semver $OLD_TAG -i $INCREMENT`
-echo $NEXT_TAG
 
 echo "git tag"
 git status
