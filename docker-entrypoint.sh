644c8333e2585d553aae8a9bb9a418d689733f07
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-12 05:20:36 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
+APP_NAME=$CI_PROJECT_NAME
+REPO_URL=$CI_PROJECT_URL
+
+env
 
 echo "git-semver-tags"
 TAGS=$(git-semver-tags)
 echo $TAGS
 
 echo "conventional-recommended-bump"
-echo $(conventional-recommended-bump)
+echo $(conventional-recommended-bump -p angular)
 
 echo "git-changelog"
-git-changelog
+git-changelog -a $APP_NAME -r $REPO_URL
 cat CHANGELOG.md
 
 echo "semver"
