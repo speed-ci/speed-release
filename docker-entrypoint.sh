7c4c0fe1af6f23572cb2954706232d4ef50f424b
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-12 21:47:47 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,15 @@ set -e
 APP_NAME=$CI_PROJECT_NAME
 REPO_URL=$CI_PROJECT_URL
 
+git config --get remote.origin.url
 git remote -v
 git status
+
+MY_REPO_URL=$(git config --get remote.origin.url | sed 's/\.git//g' | sed 's/gitlab-ci-token:xxxxxxxxxxxxxxxxxxxx@//g')
+echo $MY_REPO_URL
+
+MY_APP_NAME=${MY_REPO_URL##*/}
+echo $MY_APP_NAME
 
 echo "git-semver-tags"
 OLD_TAG=$(git-semver-tags | sed '1 ! d')
