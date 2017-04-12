dfd2445195443f15b9ad4c843d7aabc495290dc1
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-12 05:11:55 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
-echo $(git-semver-tags)
+echo "git-semver-tags"
+TAGS=$(git-semver-tags)
+echo $TAGS
+
+echo "conventional-recommended-bump"
 echo $(conventional-recommended-bump)
+
+echo "git-changelog"
 git-changelog
 cat CHANGELOG.md
+
+echo "semver"
 semver
