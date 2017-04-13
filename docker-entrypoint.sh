8f18fedc60eef829d4a9027a0555c328524be597
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-13 04:00:24 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,16 +13,14 @@ PREVIOUS_TAG=${PREVIOUS_TAG:-"0.0.0"}
 INCREMENT=$(conventional-recommended-bump -p angular)
 NEXT_TAG=`semver $PREVIOUS_TAG -i $INCREMENT`
 
-CHANGELOG=$(git-changelog -a $APP_NAME -n $NEXT_TAG -r $REPO_URL)
+git-changelog -a $APP_NAME -n $NEXT_TAG -r $REPO_URL
+CHANGELOG=$(cat CHANGELOG.md && rm CHANGELOG.md)
 
-echo "git tag"
 PROJECT_ID=`curl --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$APP_NAME" | jq .[0].id`
 LAST_COMMIT_ID=$(git log --format="%H" -n 1)
 curl --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -XPOST "$GITLAB_API_URL/projects/$PROJECT_ID/repository/tags" -d "id=$PROJECT_ID" -d "tag_name=$NEXT_TAG" -d "ref=$LAST_COMMIT_ID" -d "release_description=$CHANGELOG" | jq .
 
-
 # curl --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_ID/repository/tags" | jq .[0].name
-
 
 git status
 git clean -f
