c71ba131fb9686a64efdeb0ee5b16c6a8d5bf531
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-18 22:28:30 +0000

    chore(tag): display gitlab metadata used for git tag

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -71,9 +71,11 @@ else
     PROJECT_ID=`curl --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$APP_NAME" | jq .[0].id`
     LAST_COMMIT_ID=$(git log --format="%H" -n 1)
     
-    printinfo "INCREMENT      : $INCREMENT"
-    printinfo "NEXT_TAG       : $NEXT_TAG"
-    printinfo "LAST_COMMIT_ID : $LAST_COMMIT_ID"
+    printinfo "INCREMENT       : $INCREMENT"
+    printinfo "NEXT_TAG        : $NEXT_TAG"
+    printinfo "LAST_COMMIT_ID  : $LAST_COMMIT_ID"
+    printinfo "GITLAB_API_URL : $$GITLAB_API_URL"
+    printinfo "PROJECT_ID  : $PROJECT_ID"
     
     DATE=`(date)`
     curl --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -XPOST "$GITLAB_API_URL/projects/$PROJECT_ID/repository/tags" -d "id=$PROJECT_ID" -d "tag_name=$NEXT_TAG" -d "ref=$LAST_COMMIT_ID" -d "release_description=$CHANGELOG" -d "message=$DATE" | jq .
