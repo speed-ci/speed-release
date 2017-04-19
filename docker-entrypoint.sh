648d834785cd6603fc118941fa3c718a466dc02e
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-19 22:14:11 +0000

    chore(changelog): escape % character

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -64,7 +64,7 @@ else
     NEXT_TAG=`semver $PREVIOUS_TAG -i $INCREMENT`
     
     git-changelog -a $APP_NAME -n $NEXT_TAG -r $REPO_URL --template "/template.md"
-    CHANGELOG=$(cat CHANGELOG.md)
+    CHANGELOG=$(cat CHANGELOG.md | sed -e "s/%/\\\%/g")
     CHANGELOG=$(head -n 28 CHANGELOG.md)
     echo "release_description=$CHANGELOG"
     msee CHANGELOG.md
